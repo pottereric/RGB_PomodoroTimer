e3e63baa48b23629ba89f3a22f1d2c5ab7f26232
Eric Potter <eric.b.potter@gmail.com>
2015-12-01 07:00:46 -0500

    Code Cleanup

--- CycleThroughTheColors/CycleThroughTheColors.ino
+++ CycleThroughTheColors/CycleThroughTheColors.ino
@@ -1,16 +1,13 @@
-/*
-Adafruit Arduino - Lesson 3. RGB LED
-*/
+const int annodePin = 12;
+const int redPin = 11;
+const int bluePin = 10;
+const int greenPin = 9;
+
+const int blackBtnPin = 7;
+const int redBtnPin = 6;
+const int blackBtnPinSource = 5;
+const int redBtnPinSource = 4;
  
-int redPin = 11;
-int greenPin = 9;
-int bluePin = 10;
- 
-int blackBtnPin = 7;
-int redBtnPin = 6;
- 
-//uncomment this line if using a Common Anode LED
-#define COMMON_ANODE
 
 int delayTime = 50;
 
@@ -19,36 +16,35 @@ void setup()
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  pinMode(annodePin, OUTPUT);
+  digitalWrite(annodePin, HIGH);
+  
   pinMode(blackBtnPin, INPUT_PULLUP);
   pinMode(redBtnPin, INPUT_PULLUP);
-  pinMode(5, OUTPUT);
-  digitalWrite(5, LOW);
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
+  pinMode(blackBtnPinSource, OUTPUT);
+  digitalWrite(blackBtnPinSource, LOW);
+  pinMode(redBtnPinSource, OUTPUT);
+  digitalWrite(redBtnPinSource, LOW);
+ 
+  #ifdef SERIAL_DEBUGGING
+  Serial.begin(9600);
+  #endif
 }
  
 void loop()
 {
-  
-  
-  setColor(0, 0, 255);
+  setColor(0, 255, 255);
   delay(1000);
-  setColor(255, 0, 0);
+  setColor(255, 0, 255);
   delay(1000);
-  setColor(0, 255, 0); 
+  setColor(255, 255, 0); 
   delay(1000);
-
+  setColor(210, 100, 255); 
+  delay(1000);
 }
  
 void setColor(int red, int green, int blue)
 {
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
